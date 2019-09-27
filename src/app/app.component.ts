cf8eb33916bf9c5ffb788e8ed1c67c3a391da82e
nghut <nghut02@gmail.com>
2019-09-27 07:39:39 +0530

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { first } from 'rxjs/operators';
+
+
+export class Employee {
+  EmployeeID?: number;
+  EmployeeName?: string;
+  Age?: number;
+  DateOfBirth?: Date;
+  EmailID?: string;
+  MobileNumber?: number;
+  Address?: string;
+  City?: string;
+  State?: string;
+  IsActive?: boolean;
+  CreatedDateTime?: Date;
+  CreatedBy?: string;
+  ModifiedDateTime?: Date;
+  ModifiedBy?: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit {
+
+  constructor(private http: HttpClient) { }
   title = 'poc-ng-app-crud';
+  public Employees = [];
+
+  getAll() {
+    return this.http.get<Employee[]>(`${environment.apiUrl}/read`);
+  }
+
+  ngOnInit(): void {
+    this.getAll().pipe(first()).subscribe(Employees => {
+      this.Employees = Employees;
+      console.log(this.Employees);
+  });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import '../polyfills';
+
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {NGMaterialModule} from '../ng-material-module';
 
 @NgModule({
   declarations: [
@@ -10,9 +18,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NGMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
