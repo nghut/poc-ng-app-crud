03cad0cd8bcf221abba04030f2c30916d5a53a34
nghut <nghut02@gmail.com>
2019-09-29 22:42:04 +0530

    Crud done no validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,107 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { first } from 'rxjs/operators';
+import { isNullOrUndefined } from 'util';
+
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements  OnInit {
+
+  constructor(
+    private http: HttpClient,
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar
+    ) { }
+  title = 'Web app';
+  public Employees: IEmployee[] = [];
+
+  displayedColumns: string[] = [ 'EmployeeID'
+    , 'EmployeeName'
+    , 'DateOfBirth'
+    , 'Age'
+    , 'EmailID'
+    , 'MobileNumber'
+    , 'Address'
+    , 'City'
+    , 'State'
+  ];
+  dataSource = new MatTableDataSource();
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  // Funtions
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  getEmployees() {
+    return this.http.get<IEmployee[]>(`${environment.apiUrl}/read`);
+  }
+
+  deleteEmployee(d) {
+    return this.http.post<IStatus>(`${environment.apiUrl}/delete`, d);
+  }
+
+  onLoadEmployee() {
+    this.getEmployees().pipe(first()).subscribe(data => {
+      this.dataSource.data = data;
+  });
+  }
+
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.onLoadEmployee();
+  }
 
 
-export class Employee {
+  // Events
+  // Add Employee
+  openAddEmpDialog(d): void {
+    let rowData: IEmployee = {};
+    if (d !== '') {
+      rowData = d;
+    }
+    const dialogRef = this.dialog.open(AddEmpDialogComponent, {
+      width: '750px',
+      data: rowData
+    });
+    dialogRef.afterClosed().subscribe(result => {
+       this.onLoadEmployee();
+    });
+  }
+
+  onClickDeleteEmployee(d) {
+    const employeeID: any = { EmployeeID : d };
+    this.deleteEmployee(employeeID).pipe(first()).subscribe(data => {
+      const jsonData = data;
+      if (jsonData.Code === 1) {
+        this.openSnackBar(jsonData.Message, '');
+        this.onLoadEmployee();
+      } else {
+        this.openSnackBar(jsonData.Message, '');
+      }
+  });
+  }
+}
+
+
+export interface IStatus {
+  Code?: number;
+  Message?: string;
+}
+
+export interface IEmployee {
   EmployeeID?: number;
   EmployeeName?: string;
   Age?: number;
@@ -22,26 +119,59 @@ export class Employee {
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-add-emp-dialog',
+  templateUrl: './Shared/add-emp-dialog.component.html',
 })
-export class AppComponent implements  OnInit {
+export class AddEmpDialogComponent {
+  constructor(
+    private http: HttpClient,
+    private snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<AddEmpDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: IEmployee
+    ) {}
 
-  constructor(private http: HttpClient) { }
-  title = 'poc-ng-app-crud';
-  public Employees = [];
-
-  getAll() {
-    return this.http.get<Employee[]>(`${environment.apiUrl}/read`);
+    // Method
+  updateEmployee(d) {
+    return this.http.post<IStatus>(`${environment.apiUrl}/update`, d);
   }
 
-  ngOnInit(): void {
-    this.getAll().pipe(first()).subscribe(Employees => {
-      this.Employees = Employees;
-      console.log(this.Employees);
+  addEmployee(d) {
+    return this.http.post<IStatus>(`${environment.apiUrl}/create`, d);
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  onClickUpdateEmployee(d) {
+    if (isNullOrUndefined(d.EmployeeID)) {
+      d.DateOfBirth = null;
+      this.addEmployee(d).pipe(first()).subscribe(data => {
+        const jsonData = data;
+        if (jsonData.Code === 1) {
+          this.openSnackBar(jsonData.Message, '');
+          this.onClickCloseDialog();
+        } else {
+          this.openSnackBar(jsonData.Message, '');
+        }
+    });
+    } else {
+    this.updateEmployee(d).pipe(first()).subscribe(data => {
+      const jsonData = data;
+      if (jsonData.Code === 1) {
+        this.openSnackBar(jsonData.Message, '');
+        this.onClickCloseDialog();
+      } else {
+        this.openSnackBar(jsonData.Message, '');
+      }
   });
+}
+
   }
 
-
+    onClickCloseDialog(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, AddEmpDialogComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import '../polyfills';
 
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
+import {MatNativeDateModule, MAT_LABEL_GLOBAL_OPTIONS} from '@angular/material/core';
 import {NGMaterialModule} from '../ng-material-module';
 
+import { BlockUIModule } from 'ng-block-ui';
+import { BlockUIHttpModule } from 'ng-block-ui/http';
+
+
 @NgModule({
+  entryComponents: [AddEmpDialogComponent],
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmpDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -25,8 +31,15 @@ import {NGMaterialModule} from '../ng-material-module';
     NGMaterialModule,
     MatNativeDateModule,
     ReactiveFormsModule,
+    BlockUIModule.forRoot({
+      delayStop: 500,
+      message: 'Loading...'
+    }),
+    BlockUIHttpModule.forRoot({
+      blockAllRequestsInProgress: true
+    }), // Import Block UI Http Module
   ],
-  providers: [],
+  providers: [{provide: MAT_LABEL_GLOBAL_OPTIONS, useValue: {float: 'always'}}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
